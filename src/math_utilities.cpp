c7f29c0c3675ba7498726e7eb3a77699fb3ede11
lifan <lifan@lifan.local>
2015-07-12 11:58:42 +0800

    vimdemo: verify primality number by a simple but slow method. sparrowli

--- src/math_utilities.cpp
+++ src/math_utilities.cpp
@@ -10,3 +10,18 @@ int MathUtilities::Factorial(int n)
 
 	return result;
 }
+
+bool MathUtilities::IsPrime(int n)
+{
+	if (n <= 1) return false;
+
+	if (n % 2 == 0) return n==2;
+
+	for (int i = 3; ; i += 2) {
+		if (i > n/i) break;
+
+		if (n % i == 0) return false;
+	}
+
+	return true;
+}
--- src/math_utilities.h
+++ src/math_utilities.h
@@ -5,5 +5,6 @@ class MathUtilities
 {
 public:
 	int Factorial(int n);
+	bool IsPrime(int n);
 };
 #endif // _MATH_UTILITIES_H_
